49150131bce99b68380489d3221bc6ee2aac0c9e
selva <selvakumar1995sri@gmail.com>
2022-12-21 08:27:53 +0530

    commit git comments with explanation

--- demo.py
+++ demo.py
@@ -1,1 +1,15 @@
 print("hello dhana")
+
+git_class = {
+    "git_init": "to initialize git in local folder",
+    "git_add_filename": "to stage the file ",
+    "git_commit": "git commit -m 'commiting the demo app'",
+    "github_connection": "git remote add origin https://github.com/SelvaKumar1995sri/demo-repository.git",
+    "git_creat_branch": "git branch -M main",
+    "git_push": "git push -u origin main",
+    "creat_branch": "git branch BranchName",
+    "list_branch": "git branch",
+    "branch_change": "git checkout branchname"
+    }
+
+print(git_class)
