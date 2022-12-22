0f6f1377e777456678ed04542b55ac392793fb93
selva <selvakumar1995sri@gmail.com>
2022-12-22 08:46:36 +0530

    reset changes

--- demo.py
+++ demo.py
@@ -1,33 +1,3 @@
 print("hello dhana")
 
 print("")
-git_class = {
-    "git_init": "to initialize git in local folder",
-    "git_add_filename": "to stage the file ",
-    "git_commit": "git commit -m 'commiting the demo app'",
-    "github_connection": "git remote add origin https://github.com/SelvaKumar1995sri/demo-repository.git",
-    "git_creat_branch": "git branch -M main",
-    "git_push": "git push -u origin main",
-    "creat_branch": "git branch BranchName",
-    "list_branch": "git branch",
-    "branch_change": "git checkout branchname"
-    }
-
-print(git_class)
-
-git_second_class = {
-    "connection_string": "how to connect git to github repository",
-    "branch_creation": "creating branch in same repository",
-    "git_pull": "pulling update source code from github",
-    "git_checkout_branchname": "how to change one branch to another branch with cmd (git checkout new branch name)",
-    "github_code_edit": "we can edit code in github with editor",
-
-
-}
-
-git_class["git_pull"] = "git pull - pull current updated source code from github to git"
-
-print(git_class)
-
-
-
